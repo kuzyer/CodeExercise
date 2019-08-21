9d11b49334445605d7eeb61c6c3213197a5b2266
kuzyer <woyao068@gmail.com>
2019-08-21 22:54:30 +0800

    两数之和

---
+++ leetcode/src/test001.java
@@ -0,0 +1,19 @@
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+//两数之和
+public class test001 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
