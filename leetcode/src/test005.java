c3f354b050fe07edcf816b4c6e9ae7abea81a6ce
kuzyer <woyao068@gmail.com>
2019-08-21 22:58:20 +0800

    最长回文子串

---
+++ leetcode/src/test005.java
@@ -0,0 +1,26 @@
+//最长回文子串
+public class test005 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); //单核回文（子串长度为奇数）
+            int len2 = expandAroundCenter(s, i, i + 1); //双核回文（子串长度为偶数）
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right){
+        int L = left, R = right;
+        while(L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
