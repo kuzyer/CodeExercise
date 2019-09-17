a899f2e09a26ba34b22a95a7b98c2ecf6a9aa731
kuzyer <woyao068@gmail.com>
2019-09-17 09:42:39 +0800

    合并两个有序链表

---
+++ leetcode/src/test021.java
@@ -0,0 +1,28 @@
+//合并两个有序链表
+public class test021 {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(1);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(4);
+
+        ListNode b = new ListNode(1);
+        b.next = new ListNode(3);
+        b.next.next = new ListNode(4);
+
+        mergeTwoLists(a, b);
+    }
+}
