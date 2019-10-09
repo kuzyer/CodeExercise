3344e4238e337c3fbe624e9c4d2c4db228c90b9e
kuzyer <woyao068@gmail.com>
2019-10-09 11:30:21 +0800

    删除排序链表中的重复元素

---
+++ leetcode/src/test083.java
@@ -0,0 +1,14 @@
+//删除排序链表中的重复元素
+public class test083 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
