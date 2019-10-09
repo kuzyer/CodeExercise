1ac5da087bfb8a1b15d91e0590a91e8163d5b87c
kuzyer <woyao068@gmail.com>
2019-10-09 09:28:34 +0800

    报数

--- leetcode/src/test038.java
+++ leetcode/src/test038.java
@@ -1,5 +1,9 @@
 //报数
 public class test038 {
+    /*
+        第n项的数只是解释n-1项的情况，不进行值的加法计算
+        如第5项中，解释第4项，有1个1，1个2，2个1，因此得111221
+    */
     public String countAndSay(int n) {
         String str = "1";
         for (int i = 2; i <= n; i++) {
