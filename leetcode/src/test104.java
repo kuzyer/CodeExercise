17f192ee12566ee2645bb4bdba008442616d4cca
kuzyer <woyao068@gmail.com>
2019-10-09 15:33:00 +0800

    二叉树的最大深度

---
+++ leetcode/src/test104.java
@@ -0,0 +1,7 @@
+//二叉树的最大深度
+public class test104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
