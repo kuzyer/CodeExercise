40c50f11a78990d84c156cde7347b4b305cc8d31
kuzyer <woyao068@gmail.com>
2019-10-17 09:55:16 +0800

    环形链表

---
+++ leetcode/src/test141.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//环形链表
+public class test141 {
+    //哈希表 HashSet upup contains复杂度为n
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    //List contains复杂度为logn
+    public boolean hasCycle2(ListNode head) {
+        List<ListNode> nodesSeen = new ArrayList<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    //双指针
+    public boolean hasCycle3(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
